d082c6e64efe086092cebe92f613ac49258213b9
Daniel Petti <djpetti@gmail.com>
2024-06-28 16:49:57 -0400

    Use mean aggregation for GNN, which works better.

--- src/cotton_flower_mot/pipelines/model_training/gcnn_model.py
+++ src/cotton_flower_mot/pipelines/model_training/gcnn_model.py
@@ -224,6 +224,7 @@ def _build_gnn(
         config.num_node_features,
         config.num_edge_features,
         activation="relu",
+        agg="mean",
     )((node_features, graph_structure, edge_features))
 
     nodes1_1 = layers.BatchNormalization()(nodes1_1)
@@ -232,6 +233,7 @@ def _build_gnn(
         config.num_node_features,
         config.num_edge_features,
         activation="relu",
+        agg="mean",
     )((nodes1_1, graph_structure, edges1_1))
 
     return nodes1_2
