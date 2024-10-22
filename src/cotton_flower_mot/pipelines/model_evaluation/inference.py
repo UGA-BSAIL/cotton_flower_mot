35862673acdf42a3565d404461e95d95ee30e6a4
Daniel Petti <djpetti@gmail.com>
2024-10-22 15:02:51 -0400

    Increase the maximum number of detections.
    This is necessary for boll detections.

--- src/cotton_flower_mot/pipelines/model_evaluation/inference.py
+++ src/cotton_flower_mot/pipelines/model_evaluation/inference.py
@@ -56,7 +56,7 @@ def _filter_detections(
         nms_indices = tf.image.non_max_suppression(
             boxes,
             confidence,
-            max_output_size=15,
+            max_output_size=100,
             iou_threshold=nms_iou_threshold,
             score_threshold=confidence_threshold,
         )
